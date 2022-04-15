f2dfa16bc977b7275025a36fc054dd657a8ac944
Alexandr Lvov <iJullic@yandex.ru>
2022-04-15 23:16:02 +0300

    add local storage

---
+++ src/services/localStorage.js
@@ -0,0 +1,25 @@
+export const getDataFromLocalStorage = (filter) => {
+    return JSON.parse(localStorage.getItem(filter));
+}
+
+export const setDataToLocalStorage = (filter, data) => {
+    localStorage.setItem(filter, JSON.stringify(data));
+}
+
+export const checkRecipeInLocalStorage = (recipe, filter, changeableFilter) => {
+    const recipes = getDataFromLocalStorage(filter) || [];
+    for (let i = 0; i < recipes.length; i++) {
+        if (recipes[i].id === recipe.id) {
+            if (filter === changeableFilter) {
+                recipes.splice(i, 1);
+                setDataToLocalStorage(filter, recipes);
+                return;
+            }
+            recipes[i][changeableFilter] = !recipes[i][changeableFilter];
+            setDataToLocalStorage(filter, recipes);
+            return;
+        }
+    }
+    recipes.push({...recipe, [changeableFilter]: !recipe[changeableFilter]});
+    setDataToLocalStorage(filter, recipes);
+}
